5d849713fad562be1e3a543c1737f9eb4bec9f42
Jeremy Flowers <rpgjuwangaming@gmail.com>
2020-09-28 15:29:18 -0500

    Added Character and Player classes. Added Item struct.

---
+++ Character.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace HelloWorld
+{
+    class Character
+    {
+        private float _health;
+        private string _name;
+        protected float _damage;
+
+        public Character()
+        {
+            _health = 100;
+            _name = "Hero";
+            _damage = 10;
+        }
+
+        public Character(float healthVal, string nameVal, float damageVal)
+        {
+            _health = healthVal;
+            _name = nameVal;
+            _damage = damageVal;
+        }
+
+        public virtual float Attack(Character enemy)
+        {
+            float damageTaken = enemy.TakeDamage(_damage);
+            return damageTaken;
+        }
+
+        public void PrintStats()
+        {
+            Console.WriteLine("Name: " + _name);
+            Console.WriteLine("Health: " + _health);
+            Console.WriteLine("Damage: " + _damage);
+        }
+
+        public virtual float TakeDamage(float damageVal)
+        {
+            _health -= damageVal;
+            if (_health < 0)
+            {
+                _health = 0;
+            }
+            return damageVal;
+        }
+
+        public virtual void Save(StreamWriter writer)
+        {
+            //Save the characters stats
+            writer.WriteLine(_name);
+            writer.WriteLine(_health);
+            writer.WriteLine(_damage);
+        }
+
+        public virtual bool Load(StreamReader reader)
+        {
+            //Create variables to store loaded data.
+            string name = reader.ReadLine();
+            float damage = 0;
+            float health = 0;
+            //Checks to see if loading was successful.
+            if (float.TryParse(reader.ReadLine(), out health) == false)
+            {
+                return false;
+            }
+            if (float.TryParse(reader.ReadLine(), out damage) == false)
+            {
+                return false;
+            }
+            //If successful, set update the member variables and return true.
+            _name = name;
+            _damage = damage;
+            _health = health;
+            return true;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public bool GetIsAlive()
+        {
+            return _health > 0;
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Player
+    {
+        private Item[] _inventory;
+        private Item _currentWeapon;
+        private Item _hands;
+
+        public Player() : base()
+        {
+            _inventory = new Item[3];
+            _hands.name = "These hands";
+            _hands.statBoost = 0;
+        }
+
+        public Player(string nameVal, float healthVal, float damageVal, int inventorySize)
+            : base(healthVal, nameVal, damageVal)
+        {
+            _inventory = new Item[inventorySize];
+            _hands.name = "These hands";
+            _hands.statBoost = 0;
+        }
+
+        public Item[] GetInventory()
+        {
+            return _inventory;
+        }
+
+        public void AddItemToInventory(Item item, int index)
+        {
+            _inventory[index] = item;
+        }
+
+        public bool Contains(int itemIndex)
+        {
+            if (itemIndex > 0 && itemIndex < _inventory.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void EquipItem(int itemIndex)
+        {
+            if (Contains(itemIndex))
+            {
+                _currentWeapon = _inventory[itemIndex];
+            }
+        }
+
+        public void UnequipItem()
+        {
+            _currentWeapon = _hands;
+        }
+
+        public override float Attack(Character enemy)
+        {
+            float totalDamage = _damage + _currentWeapon.statBoost;
+            return enemy.TakeDamage(totalDamage);
+        }
+    }
+}
